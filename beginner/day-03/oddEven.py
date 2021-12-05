7dfb7c6f7c140245ca614483dc0bc21b139f3f7e
nomanme <abdullah.noman083@gmail.com>
2021-12-06 00:00:15 +0600

    update

--- beginner/day-03/oddEven.py
+++ beginner/day-03/oddEven.py
@@ -3,7 +3,7 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
-if number % 2:
+if number % 2 == 0:
     print("This is an odd number.")
 else:
     print("This is an even number.")
